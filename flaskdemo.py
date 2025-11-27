8d419d80599b8ffe18d5ed26c86af58be0ade3bb
guoyang <guoyang.zhang@my.jcu.edu.au>
2025-11-27 14:47:28 +0800

    Included original Flask demo file for reference alongside modified project version.

--- flaskdemo.py
+++ flaskdemo.py
@@ -1,63 +1,38 @@
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, render_template, request
 import wikipedia
 
 app = Flask(__name__)
-# This Flask app sets app.secret_key, which is an encryption key used "to sign cookies and other things".
-# Our app will work without it, but not completely. Without the secret key, we receive an error when searching:
-# RuntimeError: The session is unavailable because no secret key was set.
-# Storing secrets in plain code like this is not good practice. We know it.
-# We don't want you to think we're endorsing this practice!
-app.secret_key = 'IT@JCUA0Zr98j/3yXa R~XHH!jmN]LWX/,?RT'
+app.secret_key = "abc123"
 
 
-@app.route('/')
+@app.route("/")
 def home():
-    """Home page route."""
     return render_template("home.html")
 
 
-@app.route('/about')
-def about():
-    """About page route."""
-    return "I am still working on this"
-
-
-@app.route('/search', methods=['POST', 'GET'])
+@app.route("/search", methods=["GET", "POST"])
 def search():
-    """Search page route. Return either form page to search, or search results."""
-    if request.method == 'POST':
-        session['search_term'] = request.form['search']
-        return redirect(url_for('results'))
-    return render_template("search.html")
+    page = None
+    error = None
+    options = None
+
+    if request.method == "POST":
+        query = request.form["search"]
+
+        try:
+            page = wikipedia.page(query, autosuggest=False)
+        except wikipedia.DisambiguationError as e:
+            options = e.options
+        except wikipedia.PageError:
+            error = f"Page '{query}' not found. Try another title."
+
+    return render_template("results.html", page=page, error=error, options=options)
 
 
-@app.route('/results')
-def results():
-    """Results page route. Render the search results."""
-    search_term = session['search_term']
-    page = get_page(search_term)
-    return render_template("results.html", page=page)
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 
-def get_page(search_term):
-    """Get a Wikipedia page object based on the search term."""
-    # This function is not a route
-    try:
-        page = wikipedia.page(search_term)
-    except wikipedia.exceptions.PageError:
-        # No such page, so return a random one
-        page = wikipedia.page(wikipedia.random())
-    except wikipedia.exceptions.DisambiguationError:
-        # This is a disambiguation page; get the first real page (close enough)
-        page_titles = wikipedia.search(search_term)
-        # Sometimes the next page has the same name (different caps), so don't try the same again
-        if page_titles[1].lower() == page_titles[0].lower():
-            title = page_titles[2]
-        else:
-            title = page_titles[1]
-        page = get_page(wikipedia.page(title))
-    return page
-
-
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    app.run(debug=True)
